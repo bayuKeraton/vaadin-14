90672da3c60959f050d8c82ddd92d5dc2a3d178e
Galih <galih@teavaro.com>
2023-02-22 10:33:40 +0700

    first commit

---
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -0,0 +1,55 @@
+package com.example.application.views.list;
+
+import com.example.application.data.entity.Contact;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+@PWA(name = "Flow CRM Tutorial", shortName = "Flow CRM Tutorial", enableInstallPrompt = false)
+@Theme(themeFolder = "flowcrmtutorial")
+@PageTitle("Contacts | Vaadin CRM")
+@Route(value = "")
+public class ListView extends VerticalLayout {
+
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
+
+    public ListView() {
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(getToolbar(), grid);
+
+    }
+
+//    setup value for the table
+    private void configureGrid() {
+        grid.addClassNames("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+//    setup the button in the upper of the table
+    private HorizontalLayout getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button addContactButton = new Button("Add contact");
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+}
